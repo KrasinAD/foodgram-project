be2ea72513a0121e21af92f5c26012a74669be35
Krasin Anton <kr.toxa37@yandex.ru>
2023-10-19 21:23:32 +0300

    del_commit

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,5 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
+    """Кастомная пагинация проекта."""
     page_size = 6
     page_size_query_param = 'limit'
